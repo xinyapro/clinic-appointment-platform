f8706ba575309c28b730570b4d8c592509540c54
Zhu Xinya <xinya.zhuu@gmail.com>
2026-08-03 17:13:11 +0800

    Initialize Spring Boot project and add hello endpoint

---
+++ src/main/java/sg/com/xinyapro/clinic/ClinicAppointmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package sg.com.xinyapro.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAppointmentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAppointmentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/com/xinyapro/clinic/ClinicAppointmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.xinyapro.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppointmentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
